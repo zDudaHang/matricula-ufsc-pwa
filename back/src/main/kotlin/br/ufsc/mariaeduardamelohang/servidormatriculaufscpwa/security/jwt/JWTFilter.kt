901eca8d84002b9efb46d6b895acf9abc5dcd791
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2022-09-04 11:58:52 -0300

    Fazendo a autorização funcionar

--- back/src/main/kotlin/br/ufsc/mariaeduardamelohang/servidormatriculaufscpwa/security/jwt/JWTFilter.kt
+++ back/src/main/kotlin/br/ufsc/mariaeduardamelohang/servidormatriculaufscpwa/security/jwt/JWTFilter.kt
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
-import java.util.UUID
 import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
@@ -27,17 +26,18 @@ class JWTFilter(
         val accessToken: String? = getTokenFromHeader(request)
         if (accessToken != null) {
             if (jwtService.isAccessTokenValid(accessToken)) authenticate(accessToken)
-        } else {
-            filterChain.doFilter(request, response)
         }
+        filterChain.doFilter(request, response)
     }
 
     private fun authenticate(tokenFromHeader: String) {
         val matricula = jwtService.getMatricula(tokenFromHeader)
         if (matricula != null) {
             val aluno = alunoService.buscarAlunoPelaMatricula(matricula)
-            val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(aluno, null, null)
-            SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+            if (aluno != null) {
+                val alunoAutenticado = UsernamePasswordAuthenticationToken(aluno, null, null)
+                SecurityContextHolder.getContext().authentication = alunoAutenticado
+            }
         }
     }
 
