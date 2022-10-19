dabfa94c39a9f8e11f4de51b82addafc05aa8e55
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2022-10-19 13:56:21 -0300

    Deixando a informação mais clara sobre o IAA

--- front/src/components/PosicaoInfoAlert.tsx
+++ front/src/components/PosicaoInfoAlert.tsx
@@ -5,7 +5,8 @@ export function PosicaoInfoAlert() {
   const { iaa } = useAuthContext()
   return (
     <Alert type='info' inline>
-      A posição em uma turma é determinada pelo Índice de Aproveitamento Semestral Acumulado (IAA). O seu é {iaa}
+      A posição em uma turma é determinada pelo Índice de Aproveitamento Semestral Acumulado (IAA) e o seu é{' '}
+      <strong>{iaa}</strong>. Esse valor foi gerado <strong> automaticamente </strong> pelo servidor
     </Alert>
   )
 }
